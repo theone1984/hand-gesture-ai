08a6001aa8cead65e93bf1d404ceadd65e297693
Christian Geishauser <christian.geishauser@tngtech.com>
2019-02-03 12:58:44 +0100

    cv2 output

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ import scipy.misc
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from nets.ColorHandPose3DNetwork import ColorHandPose3DNetwork
+from nets.ColorHandPose3DNetwork import ColorHandPose3DNetwork, plot_hand_cv2
 from utils.general import detect_keypoints, trafo_coords, plot_hand, plot_hand_3d
 
 
@@ -63,11 +63,10 @@ def main(args):
 
     while True:
         _, image_raw = inputStream.read()
-        image_rgb = cv2.cvtColor(image_raw,cv2.COLOR_BGR2RGB)
+        image_resized = cv2.resize(image_raw, (320, 240))
+        image_rgb = cv2.cvtColor(image_resized,cv2.COLOR_BGR2RGB)
 
-        image_resized = cv2.resize(image_rgb, (320, 240))
-        image_v = np.expand_dims((image_resized.astype('float') / 255.0) - 0.5, 0)
-
+        image_v = np.expand_dims((image_rgb.astype('float') / 255.0) - 0.5, 0)
 
         start_time = time.time()
         hand_scoremap_v, image_crop_v, scale_v, center_v,\
@@ -77,34 +76,16 @@ def main(args):
         delta_time = time.time() - start_time
         print("Inference time: " + str(delta_time))
 
-        hand_scoremap_v = np.squeeze(hand_scoremap_v)
-        image_crop_v = np.squeeze(image_crop_v)
-        keypoints_scoremap_v = np.squeeze(keypoints_scoremap_v)
-        keypoint_coord3d_v = np.squeeze(keypoint_coord3d_v)
-
         # post processing
-        image_crop_v = ((image_crop_v + 0.5) * 255).astype('uint8')
+        keypoints_scoremap_v = np.squeeze(keypoints_scoremap_v)
         coord_hw_crop = detect_keypoints(np.squeeze(keypoints_scoremap_v))
         coord_hw = trafo_coords(coord_hw_crop, center_v, scale_v, 256)
 
-        # visualize
-        fig = plt.figure(1)
-        ax1 = fig.add_subplot(221)
-        ax2 = fig.add_subplot(222)
-        ax3 = fig.add_subplot(223)
-        ax4 = fig.add_subplot(224, projection='3d')
-        ax1.imshow(image_resized)
-        plot_hand(coord_hw, ax1)
-        ax2.imshow(image_crop_v)
-        plot_hand(coord_hw_crop, ax2)
-        ax3.imshow(np.argmax(hand_scoremap_v, 2))
-        plot_hand_3d(keypoint_coord3d_v, ax4)
-        ax4.view_init(azim=-90.0, elev=-90.0)  # aligns the 3d coord with the camera view
-        ax4.set_xlim([-3, 3])
-        ax4.set_ylim([-3, 1])
-        ax4.set_zlim([-3, 3])
-        plt.show()
+        plot_hand_cv2(image_resized, coord_hw)
+        image_fullsize = cv2.resize(image_resized, (1600, 1200))
 
+        cv2.imshow('result', image_fullsize)
+        cv2.waitKey(1)
 
     cv2.releaseAllWindows()
     return 0
@@ -113,66 +94,3 @@ def main(args):
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
-
-
-
-if __name__ == '__main__':
-    # images to be shown
-    image_list = list()
-    image_list.append('./data/img.png')
-
-    # network input
-    image_tf = tf.placeholder(tf.float32, shape=(1, 240, 320, 3))
-    hand_side_tf = tf.constant([[1.0, 0.0]])  # left hand (true for all samples provided)
-    evaluation = tf.placeholder_with_default(True, shape=())
-
-    # build network
-    net = ColorHandPose3DNetwork()
-    hand_scoremap_tf, image_crop_tf, scale_tf, center_tf,\
-    keypoints_scoremap_tf, keypoint_coord3d_tf = net.inference(image_tf, hand_side_tf, evaluation)
-
-    # Start TF
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
-    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-
-    # initialize network
-    net.init(sess)
-
-    # Feed image list through network
-    for img_name in image_list:
-        image_raw = scipy.misc.imread(img_name)
-        image_raw = scipy.misc.imresize(image_raw, (240, 320))
-        image_v = np.expand_dims((image_raw.astype('float') / 255.0) - 0.5, 0)
-
-        hand_scoremap_v, image_crop_v, scale_v, center_v,\
-        keypoints_scoremap_v, keypoint_coord3d_v = sess.run([hand_scoremap_tf, image_crop_tf, scale_tf, center_tf,
-                                                             keypoints_scoremap_tf, keypoint_coord3d_tf],
-                                                            feed_dict={image_tf: image_v})
-
-        hand_scoremap_v = np.squeeze(hand_scoremap_v)
-        image_crop_v = np.squeeze(image_crop_v)
-        keypoints_scoremap_v = np.squeeze(keypoints_scoremap_v)
-        keypoint_coord3d_v = np.squeeze(keypoint_coord3d_v)
-
-        # post processing
-        image_crop_v = ((image_crop_v + 0.5) * 255).astype('uint8')
-        coord_hw_crop = detect_keypoints(np.squeeze(keypoints_scoremap_v))
-        coord_hw = trafo_coords(coord_hw_crop, center_v, scale_v, 256)
-
-        # visualize
-        fig = plt.figure(1)
-        ax1 = fig.add_subplot(221)
-        ax2 = fig.add_subplot(222)
-        ax3 = fig.add_subplot(223)
-        ax4 = fig.add_subplot(224, projection='3d')
-        ax1.imshow(image_raw)
-        plot_hand(coord_hw, ax1)
-        ax2.imshow(image_crop_v)
-        plot_hand(coord_hw_crop, ax2)
-        ax3.imshow(np.argmax(hand_scoremap_v, 2))
-        plot_hand_3d(keypoint_coord3d_v, ax4)
-        ax4.view_init(azim=-90.0, elev=-90.0)  # aligns the 3d coord with the camera view
-        ax4.set_xlim([-3, 3])
-        ax4.set_ylim([-3, 1])
-        ax4.set_zlim([-3, 3])
-        plt.show()
